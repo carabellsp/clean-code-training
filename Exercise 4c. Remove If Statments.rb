4a84b7f7aa92b51ffbc1827ea607c28be2789e89
Cara Bell <cara.bell@192.168.1.131>
2021-10-12 16:31:11 +0100

    use explicit return over multiple if conditions

--- Exercise 4c. Remove If Statments.rb
+++ Exercise 4c. Remove If Statments.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 def benefit_rate
-  if (employee_on_vacation?)
-    if length_of_vacation > 1.week
-      return 0.5
-    end
-  end
+  return 0.5 if employee_on_vacation? && length_of_vacation > 1.week
 
   1.0
 end
+
+# assuming there is an employee_on_vacation? method & length_of_vacation variable stored elsewhere in the codebase
