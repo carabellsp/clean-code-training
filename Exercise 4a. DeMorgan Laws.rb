b731e19543aa95d6e86fe386c54093edeb886676
Cara Bell <cara.bell@192.168.1.131>
2021-10-12 16:12:36 +0100

    use demorgan laws to ensure better readability of conditionals

--- Exercise 4a. DeMorgan Laws.rb
+++ Exercise 4a. DeMorgan Laws.rb
@@ -4,8 +4,13 @@ class PhoneNumberFormatValidator < ActiveModel::Validator
   PHONE_REGEX = /^[0-9]{9}$/
 
   def validate(record)
-    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+    if record.phone_number.present? && !(record.phone_number =~ PHONE_REGEX)
       record.errors[:phone_number] << "invalid phone number format"
     end
   end
 end
+
+# originally
+# unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+
+# could not find how to do reverse of =~ without wrapping in brackets and using !
