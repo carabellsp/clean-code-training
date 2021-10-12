d1e668a9fa33125c54d4bd0ddc44fd793892d629
Cara Bell <cara.bell@192.168.1.131>
2021-10-12 16:16:51 +0100

    extract conditions into separate method

--- Exercise 4b. Extract Conditions.rb
+++ Exercise 4b. Extract Conditions.rb
@@ -4,8 +4,14 @@ class PhoneNumberFormatValidator < ActiveModel::Validator
   PHONE_REGEX = /^[0-9]{9}$/
 
   def validate(record)
-    unless record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+    unless phone_number_valid_or_missing(record)
       record.errors[:phone_number] << "invalid phone number format"
     end
   end
-end 
+
+  private
+
+  def phone_number_valid_or_missing(record)
+    record.phone_number.nil? || record.phone_number =~ PHONE_REGEX
+  end
+end
